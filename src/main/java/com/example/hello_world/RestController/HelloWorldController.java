e0e23c3e1d567f8c87d59bea7e3e21fcfb911ba8
wissemHafyenne <wissemhafyenne09@gmail.com>
2025-08-07 08:08:57 +0100

    version_0

---
+++ src/main/java/com/example/hello_world/RestController/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.hello_world.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+
+
+
+}
